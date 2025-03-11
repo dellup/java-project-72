291b8cdefc92dc9a3a8a53ef037f2e1b450c4959
Danil Zenin <zenindanil2000@gmail.com>
2025-03-12 01:55:26 +0300

    fix app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,10 +8,16 @@ import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 public class App {
     public static void main(String[] args) throws SQLException, IOException {
         var app = getApp();
@@ -20,6 +26,13 @@ public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7060");
         return Integer.valueOf(port);
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
     }
 
     private static String getDatabaseUrl() {
@@ -39,7 +52,14 @@ public class App {
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(getDatabaseUrl());
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-        BaseRepository.dataSource = dataSource;;
+        var sql = readResourceFile("schema.sql");
+
+        log.info(sql);
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
