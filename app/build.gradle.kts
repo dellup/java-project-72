af53bfb35775c897ca1973f32e0acb8a3af67955
Danil Zenin <zenindanil2000@gmail.com>
2025-03-11 18:07:56 +0300

    upgrade build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
+    application
+    checkstyle
+    id("io.freefair.lombok") version "8.6"
     id("java")
+
+}
+
+application {
+    mainClass.set("hexlet.code.Main")
 }
 
 group = "hexlet.code"
@@ -10,7 +18,18 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("com.h2database:h2:2.3.232")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("org.apache.commons:commons-text:1.11.0")
+    implementation("gg.jte:jte:3.1.16")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.6")
+
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation(platform("org.junit:junit-bom:5.11.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
