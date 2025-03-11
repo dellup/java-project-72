a5562726f724fb21f7c8c4ae0802f0dd1619c73f
Danil Zenin <zenindanil2000@gmail.com>
2025-03-11 19:33:43 +0300

    add dockerfile

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+
+public class App {
+    public static void main(String[] args) {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7060");
+        return Integer.valueOf(port);
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=utf-8");
+        });
+
+        app.get("/", ctx -> {
+           ctx.result("Hello, World");
+        });
+
+        return app;
+    }
+}
