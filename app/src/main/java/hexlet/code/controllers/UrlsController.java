6e01bc526f30436421e2651cbc0e8cfabff174d9
Danil Zenin <zenindanil2000@gmail.com>
2025-03-18 12:45:50 +0300

    add urls files

---
+++ app/src/main/java/hexlet/code/controllers/UrlsController.java
@@ -0,0 +1,60 @@
+package hexlet.code.controllers;
+
+import hexlet.code.dto.UrlsPage;
+import hexlet.code.model.Url;
+import hexlet.code.util.NamedRoutes;
+import io.javalin.http.Context;
+import hexlet.code.repository.UrlRepository;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+public class UrlsController {
+    public static void createUrl(Context ctx) throws SQLException {
+        var input = ctx.formParam("url");
+
+        URL parsedUrl;
+        try {
+            parsedUrl = new URI(input).toURL();
+        } catch (URISyntaxException | MalformedURLException | NullPointerException | IllegalArgumentException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flashType", "danger");
+            ctx.redirect(NamedRoutes.rootPath());
+            return;
+        }
+
+        String normalizedUrl = String
+                .format(
+                        "%s://%s%s",
+                        parsedUrl.getProtocol(),
+                        parsedUrl.getHost(),
+                        parsedUrl.getPort() == -1 ? "" : ":" + parsedUrl.getPort()
+                )
+                .toLowerCase();
+
+        Url url = UrlRepository.findByName(normalizedUrl).orElse(null);
+
+        if (url == null) {
+            UrlRepository.save(new Url(normalizedUrl));
+            ctx.sessionAttribute("flash", "Страница успешно добавлена");
+            ctx.sessionAttribute("flashType", "success");
+        } else {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("flashType", "info");
+        }
+        ctx.redirect(NamedRoutes.urlsPath());
+    }
+
+    public static void showUrls(Context ctx) throws SQLException {
+        List<Url> urls = UrlRepository.getEntities();
+        UrlsPage page = new UrlsPage(urls);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flashType"));
+        ctx.render("urls/index.jte", model("page", page));
+    }
+}
